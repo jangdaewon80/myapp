7fcff515c56c59bcb7037b2dc424d4fd1f3399b0
Jang Daewon <ddaewon3704@gmail.com>
2024-07-29 17:52:15 +0900

    ok

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -1,9 +1,40 @@
 package com.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Have a nice day");
+        // Student student = new Students();
+        // System.out.println(student.getStudentId());
+        // System.out.println(student.getStudentName());
+        // student.setStudentId("1")
+        // student.setStudentName("이순신")
+
+        // ApplicationContext context = 
+        //     new ClassPathXmlApplicationContext( "Beans.xml");
+        // Student student = 
+        //     context.getBean("student", Student.class);
+        //     student.setStudentId("2");
+        //     student.setStudentName("강감찬");
+        //     System.out.println(student.getStudentName());
+           // Database db = CtreateDatabase("Mysql");
+           // Database db = CtreateDatabase("Oracle");
+
+        // Address address  = new Address();
+        // address.setLocation("Seoul");
+        // Employee employee = new Employee();
+        // employee.setName("세종대왕");
+        // employee.setAddress(address);
+        // employee.displayInfo();
+        ApplicationContext context = new ClassPathXmlApplicationContext( "Beans.xml");
+
+        Employee employee = context.getBean("employee", Employee.class);
+        employee.setName("이순신");
+        Address address = context.getBean("address", Address.class);
+        address.setLocation("Daegu");
+        employee.setAddress(address);
+        employee.displayInfo();
+
     }
 }
---
+++ src/main/java/com/test/Student.java
@@ -0,0 +1,20 @@
+package com.test;
+
+//Student studentId, studentName
+public class Student {
+    private String studentId;
+    public String getStudentId() {
+        return studentId;
+    }
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+    public String getStudentName() {
+        return StudentName;
+    }
+    public void setStudentName(String studentName) {
+        StudentName = studentName;
+    }
+    private String StudentName;
+
+}
