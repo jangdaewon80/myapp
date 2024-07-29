fa720d2f0b248cc2cd5dbd4c99a23a692fed3ea2
Jang Daewon <ddaewon3704@gmail.com>
2024-07-29 15:05:36 +0900

    ok

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -4,6 +4,6 @@ package com.test;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Have a good day4");
+        System.out.println("Have a good day6");
     }
 }
